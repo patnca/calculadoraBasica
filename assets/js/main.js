245a5e798303a9c9d19ffeb3120ede817550dc12
Patrick França <psfranca@gmail.com>
2023-05-16 21:40:57 -0300

    :tada: Projeto inicial concluído

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,55 @@
-let campo1 = parseInt(document.getElementById("campo1").value);
-let campo2 = parseInt(document.getElementById("campo2").value);
+let campo1 = document.getElementById("campo1");
+let campo2 = document.getElementById("campo2");
 let botaoSoma = document.getElementById("botao-somar");
+let botaoSubtrai = document.getElementById("botao-subtrair");
+let botaoMultiplica = document.getElementById("botao-multiplicar");
+let botaoDivide = document.getElementById("botao-dividir");
 let resultado = document.querySelector('.area-resposta');
 
 
 botaoSoma.addEventListener('click', () => {
-    let soma = campo1 + campo2;
+    let soma = +campo1.value + +campo2.value;
     let resposta = `O Resultado = ${soma}`;
     resultado.innerHTML = resposta;
     resultado.style.display = 'block';
 
-    campo1 = "";
-    campo2 = "";
+    campo1.value = "";
+    campo2.value = "";
+});
+
+botaoSubtrai.addEventListener('click', () => {
+    let sub = +campo1.value - +campo2.value;
+    let resposta = `O Resultado = ${sub}`;
+    resultado.innerHTML = resposta;
+    resultado.style.display = 'block';
+
+    campo1.value = "";
+    campo2.value = "";
+});
+
+botaoMultiplica.addEventListener('click', () => {
+    let multi = +campo1.value * +campo2.value;
+    let resposta = `O Resultado = ${multi}`;
+    resultado.innerHTML = resposta;
+    resultado.style.display = 'block';
+
+    campo1.value = "";
+    campo2.value = "";
+});
+
+botaoDivide.addEventListener('click', () => {
+    let div = +campo1.value / +campo2.value;
+    if(+campo1.value == 0 || +campo2.value == 0) {
+       let resposta = `Não podemos divir por 0`;
+       resultado.innerHTML = resposta;
+       resultado.style.display = 'block';
+    } else {
+        resposta = `O Resultado = ${div}`;
+        resultado.innerHTML = resposta;
+        resultado.style.display = 'block';
+
+    }  
+
+    campo1.value = "";
+    campo2.value = "";
 });
